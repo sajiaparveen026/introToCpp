d65ba5e05f9e4d82cda5a31c9e9477a2a43c0315
sajiaparveen026 <100033460+sajiaparveen026@users.noreply.github.com>
2023-07-05 10:11:10 +0000

    arithmetic

--- c1.cpp
+++ c1.cpp
@@ -7,33 +7,43 @@ int main()
 
     // ************************* OUTPUT
     //1. When you want to print a String or a sentence
-    cout<<"Hello Students\n";
+    // cout<<"Hello Students\n";
 
-    //2. When you want to print only a variable
-    int n = 9;
-    cout<<n;
+    // //2. When you want to print only a variable
+    // int n = 9;
+    // cout<<n;
 
-    //3. When you want to print a String with variable
-    cout<<"\nThe value of 'n' is : "<<n<<'\n';
+    // //3. When you want to print a String with variable
+    // cout<<"\nThe value of 'n' is : "<<n<<'\n';
 
-    // ********************* Input
+    // // ********************* Input
 
-    //1. When you want to take input of a variable
-    cout<<"Enter: "<<'\n';
-    int num ;
-    cin>>num;
-    cout<<num<<"\n";
+    // //1. When you want to take input of a variable
+    // cout<<"Enter: "<<'\n';
+    // int num ;
+    // cin>>num;
+    // cout<<num<<"\n";
 
-    cout<<"Enter a word: "<<'\n';
-    string word;
-    cin>>word;
-    cout<<word<<'\n';
+    // cout<<"Enter a word: "<<'\n';
+    // string word;
+    // cin>>word;
+    // cout<<word<<'\n';
 
-    //2. When you want to take input of two variables together.
-    int x,y;
-    cout<<"Enter two nums: "<<'\n';
-    cin>>x>>y;
-    cout<<"Value of x is "<<x<<" The value of y is "<<y<<'\n';
+    // //2. When you want to take input of two variables together.
+    // int x,y;
+    // cout<<"Enter two nums: "<<'\n';
+    // cin>>x>>y;
+    // cout<<"Value of x is "<<x<<" The value of y is "<<y<<'\n';
 
-    return 0;
+   //Q. tAKE a input of string data type and try to print this.
+
+    //  string n;
+    //  cin>>n;
+    //  cout<<"the value of n is" <<n<<'\n';
+    float n1, n2;
+    cin>> n1 >> n2;
+    cout<< "the value of n1 is: "<<n1<<" the value of n2 is: "<<n2<< "\n";
+
+     return 0;
+     
 }
---
+++ c2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  // Operators
+
+  //1. Arithmetic operators :- +,-,*,/,%
+     // 8+4*9/3
+     /*
+       8 + 36 / 3
+       8 + 12
+       20
+     */
+
+  // int n, m;
+  // cin>> n>> m;
+  // cout<< n + m <<"\n";
+
+// 
+
+//Q. take input of sides of rectangle , means its breadth and length and calculate it's area.
+
+// int l , b;
+// cin>> l >> b;
+// cout<< "the area of the rectangle is " << l * b << "\n";
+
+// Q. take input of celsius and convert it into farenheit
+// f=(c*9/5) +32
+// int f , c;
+// cin>> c;
+// f=(c*9/5) +32;
+// cout<< "the value of f is: " << f << "\n";
+
+// sizeof operator
+cout<<sizeof(double)<<'\n';
+    return 0;
+}
